29a66a210b7f39d0770595f6879f8d380f7480d1
YY1234hj <156610384+YY1234hj@users.noreply.github.com>
2024-01-14 22:57:40 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,4 +1,9 @@
-const linkUrl = () => {
-  const url = 'https://google.com';
-  return url;
-}
+document.addEventListener("DOMContentLoaded", function() {
+	const url = 'https://google.com';
+
+  const btns = document.querySelectorAll('.js-button');
+	if(btns.length === 0) return;
+	btns.forEach(item => {
+		item.href = url;
+	})
+});
